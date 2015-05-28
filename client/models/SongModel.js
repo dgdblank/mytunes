b9effec7c1e6908321e3670791603258e2dea62d
Hack Reactor Students <accounts@hackreactor.com>
2015-05-27 17:07:37 -0700

    Click song to add to Queue

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,14 @@ var SongModel = Backbone.Model.extend({
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  addToQueue: function() {
+    this.trigger('addToQueue', this);
+  },
+
+  removefromQueue: function() {
+    this.trigger('removeFromQueue', this);
   }
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,22 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this);
+    this.render();
   },
 
   render: function() {
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
     return this.$el;
   }
 
